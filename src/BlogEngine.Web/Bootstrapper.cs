cad7ce07a6104563e55e8ebf05662ef6f0c76ff0
Kris McGinnes <kmcginnes@me.com>
2013-05-07 19:06:52 -0500

    Imported Nancy and Bootstrap

---
+++ src/BlogEngine.Web/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+using Nancy.Conventions;
+
+namespace BlogEngine.Web
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+            nancyConventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("Scripts"));
+        }
+    }
+}
